264eca91182ca375fea436958801d1f4624e5758
Rodrigo Vitoriense <80417182+rodrigorvix@users.noreply.github.com>
2021-05-29 12:59:47 -0300

    Create ex14.js

---
+++ ex14.js
@@ -0,0 +1,13 @@
+function objetoParaArray (obj) {
+  return console.log(Object.entries(obj))
+}
+
+objetoParaArray({
+nome: "Maria",
+profissao: "Desenvolvedora de software"
+}) 
+
+objetoParaArray({
+codigo: 11111,
+preco: 12000
+}) 
