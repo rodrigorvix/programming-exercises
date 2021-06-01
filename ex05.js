fe9fd7273b1fac9e66d84d4af707f63674cb3d57
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-01 17:00:35 -0300

    Update ex05.js

--- ex05.js
+++ ex05.js
@@ -1,3 +1,8 @@
-const maiorOuIgual = (n1,n2) => console.log(n1 >= n2)
+const maiorOuIgual = (n1,n2) => {
+  if(typeof n1 != typeof n2){
+    return false
+}
+  return(console.log(n1 >= n2))
+}
 
 maiorOuIgual(0,0)
