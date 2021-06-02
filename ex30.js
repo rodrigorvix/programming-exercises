cc0ee1c38b5535e95c7a9781db682833fad59784
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-02 14:55:34 -0300

    Update ex30.js

--- ex30.js
+++ ex30.js
@@ -1,21 +1,22 @@
-function Estudante(nome, notas){
+function Estudante (nome, notas) {
   this.nome = nome
-  const somaNotas = notas.reduce((acumulador, atual) => acumulador + atual)
-  this.media = somaNotas / notas.length
+  const soma = notas.reduce((acumulador,atual) => acumulador + atual)
+  this.media = soma / notas.length
 }
-function receberMelhorEstudante (estudantes){
-  let arrayEstudantes = []
-  
-  for(let i in estudantes){
-      arrayEstudantes.push(new Estudante(i, estudantes[i]))
+
+function receberMelhorEstudante (obj) {
+  const arrayEstudantes = []
+
+  for(let i in obj) {
+    arrayEstudantes.push(new Estudante(i, obj[i]))
   }
   const medias = arrayEstudantes.map(estudante => estudante.media)
-  const melhorMedia = Math.max(...medias)
-
-  return console.log(arrayEstudantes.filter(estudante => estudante.media === melhorMedia))
+  const maiorMedia = Math.max(...medias)
+  return console.log(arrayEstudantes.filter(estudante => estudante.media === maiorMedia))
 }
+
 receberMelhorEstudante({
-  Joao: [8, 7.6, 8.9, 6], // média 7.625
-  Mariana: [9, 6.6, 7.9, 8], // média 7.875
-  Carla: [7, 7, 8, 9] // média 7.75
+  Joao: [8, 7.6, 8.9, 6], 
+  Mariana: [9, 6.6, 7.9, 8], 
+  Carla: [7, 7, 8, 9] 
   })
